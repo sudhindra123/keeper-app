7509b75b891faa2ca49071dbf2c76daf25c27b6f
sudhindra123 <114804951+sudhindra123@users.noreply.github.com>
2022-10-01 16:08:30 +0530

    Add files via upload

---
+++ build/precache-manifest.c63f1c57f1d8e28beadde3a1da3b6ca8.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed2ea385785026ae6d8b3601ba917cb6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4db286bc047176e313b0",
+    "url": "/static/js/2.c6db2bb7.chunk.js"
+  },
+  {
+    "revision": "e59caa5f542fb4c55eb5",
+    "url": "/static/js/main.468891b2.chunk.js"
+  },
+  {
+    "revision": "10e117344dea3db99e26",
+    "url": "/static/js/runtime-main.b5766938.js"
+  }
+]);
